c1fe77aae0f8fb9d00e565c4e19613b0b9fc1a9e
Andres811 <penalunandres@gmail.com>
2015-05-14 01:05:53 -0300

    Base
    DAL: Conector Base de datos
    BO: Entidades
    BL: Logica (Controladores)

---
+++ TicketSaleSolution/BL/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using DAL;
+
+namespace BL
+{
+    public class UserController
+    {
+        public User getUser(int pid)
+        {
+            User us = null;
+            try
+            {
+                using (TicketSaleEntities context = new TicketSaleEntities())
+                {
+                    us = context.User.FirstOrDefault(u => u.id == pid);
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return us;
+        }
+    }
+}
