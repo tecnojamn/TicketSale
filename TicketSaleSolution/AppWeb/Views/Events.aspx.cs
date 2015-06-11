24d1c609455f8bc5767101e76d338a8c1d96a00a
Andres811 <penalunandres@gmail.com>
2015-06-11 07:26:16 -0300

    Event Page

--- TicketSaleSolution/AppWeb/Views/Events.aspx.cs
+++ TicketSaleSolution/AppWeb/Views/Events.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using DTO;
 using System.Data;
+using COM;
 
 namespace AppWeb.Views
 {
@@ -13,6 +14,11 @@ namespace AppWeb.Views
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["log"] != null && Session["log"] == SESSION.STATE.ON)
+            {
+                bool log = true;
+            }
+
             int idEvent;
 
             if (Int32.TryParse(Request.QueryString["id"], out idEvent))
@@ -25,22 +31,26 @@ namespace AppWeb.Views
                 lblTime.Text = eventDTO.date.ToString("HH:mm");
                 lblLoc.Text = eventDTO.EventLocation.name;
                 lblTickets.Text = eventDTO.getAvailableTicketCount().ToString() + " / " + eventDTO.getTotalTicketCount().ToString();
-                
+
                 //Creo datatable para meter en el gridview
                 DataTable dtTicketType = new DataTable();
+                //Columnas
                 dtTicketType.Columns.Add("Sector");
                 dtTicketType.Columns.Add("Costo");
-                dtTicketType.Columns.Add("Entradas");                
+                dtTicketType.Columns.Add("Entradas");
+                dtTicketType.Columns.Add("Reservar");
 
+                //Creo rows
                 foreach (var tt in eventDTO.TicketType)
                 {
                     DataRow _row = dtTicketType.NewRow();
                     _row["Sector"] = tt.description;
                     _row["Costo"] = tt.cost;
                     _row["Entradas"] = tt.getAvailableTicketCount() + " / " + tt.getTotalTicketCount();
+                   // _row["Reservar"] = (new Button() { Text="+"});
                     dtTicketType.Rows.Add(_row);
                 }
-                
+
                 grdTickets.DataSource = dtTicketType;
                 grdTickets.DataBind();
             }
